95350d97db566c1c5001fff933e85ad6ad590ad9
AbdulWahab Kotb <akotb@smart.kotb>
2025-06-09 11:48:34 +0300

    Upgrade to v18 [tree to list]

--- __manifest__.py
+++ __manifest__.py
@@ -1,3 +1,6 @@
+# type: ignore
+# noqa
+
 {
     'name': 'AIO Hospital Management System',
     'version': '1.0',
@@ -9,10 +12,12 @@
     'depends': ['base', 'mail'],
     'data': [
         'security/ir.model.access.csv',
-        'views/menus.xml',
         'views/patient_view.xml',
+        'views/menus.xml',
+        
     ],
     'installable': True,
     'application': True,
     'auto_install': False,
+    'icon': 'aio_hms/static/description/icon.png',
 }
